b11bcad0d7be980d7a45817a0638004224d6949c
Sebastian Neutsch <sebastian.neutsch@t-online.de>
2018-09-22 14:12:23 +0200

    Copyright and license added

--- simulator/main.c
+++ simulator/main.c
@@ -1,13 +1,29 @@
-/*	#########################################################
- * 	#			HfTL SAFARI PROTOTYP - SIMULATOR			#
- * 	#														#
- * 	#	Author:	Sebastian Neutsch							#
- * 	#	Mail:	sebastian.neutsch@t-online.de				#
- * 	#
- * 	#
- * 	#########################################################
+/*	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+ * 	#																			#
+ * 	#				HfTL SAFARI PROTOTYP - SIMULATOR							#
+ * 	#																			#
+ * 	#	Author:		Sebastian Neutsch											#
+ * 	#	Mail:		sebastian.neutsch@t-online.de								#
+ *  #	Copyright:	2018 Sebastian Neutsch / Deutsche Telekom AG				#
+ * 	#																			#
+ * 	#																			#
+ * 	#	This file is part of safari_server.										#
+ *	#																			#
+ *	#	safari_server is free software: you can redistribute it and/or modify	#
+ *	#	it under the terms of the GNU General Public License as published by	#
+ *	#	the Free Software Foundation, either version 3 of the License, or		#
+ *	#	(at your option) any later version.										#
+ *	#																			#
+ *	#	safari_server is distributed in the hope that it will be useful,		#
+ *	#	but WITHOUT ANY WARRANTY; without even the implied warranty of			#
+ *	#	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the			#
+ *	#	GNU General Public License for more details.							#
+ *	#																			#
+ *	#	You should have received a copy of the GNU General Public License		#
+ *	#	along with safari_server. If not, see <https://www.gnu.org/licenses/>.	#
+ *	#																			#
+ * 	# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 */
-
 #include <basic.h>
 #include <socket.h>
 #include <simulator.h>
